1efc574cecfedfbfb869c1599cfb647dc414dc32
AlvinYou <arvinyou34@gmail.com>
2024-07-03 16:42:17 +0800

    create the compute_mask_mix_polygon function

--- labelme/ai/_utils.py
+++ labelme/ai/_utils.py
@@ -41,7 +41,7 @@ def compute_mask_mix_polygon(mask):
     # Find contours in the mask
     contours = skimage.measure.find_contours(np.pad(mask, pad_width=1), level=0.5)
     if len(contours) == 0:
-        logger.warning("No contour found, so returning empty polygon.")
+        logger.warning("No contour found, so returning empty polygon!")
         return np.empty((0, 2), dtype=np.float32)
     
     polygons = []

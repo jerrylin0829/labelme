f52f5cd39c9f05bef71fb249a500680d7f6476f5
Arvin You <arvinyou34@gmail.com>
2024-07-22 14:11:45 +0800

    feat.: Disable other model options

---
+++ labelme/ai/_utils.py
@@ -0,0 +1,89 @@
+import imgviz
+import numpy as np
+import skimage
+
+import onnxruntime as ort
+from labelme.logger import logger
+
+def get_available_providers(): ## added by Alvin
+    try:
+        providers = ort.get_available_providers()
+        logger.info(f"Available providers: {providers}")
+        if 'CUDAExecutionProvider' in providers:
+            logger.info("CUDAExecutionProvider is available and will be used.")
+            return ['CUDAExecutionProvider']
+    except Exception as e:
+        logger.warning(f"Error checking GPU availability: {e}")
+    logger.info("CUDAExecutionProvider is not available, using CPUExecutionProvider.")
+    return ['CPUExecutionProvider']
+
+
+def set_providers(mode): ## added by Alvin
+    if mode == "CUDA":
+        return get_available_providers()
+    elif mode == "CPU":
+        logger.info("CPU Mode Selected : CPUExecutionProvider is available and will be used.")
+    return ['CPUExecutionProvider']
+
+  
+def _get_contour_length(contour):
+    contour_start = contour
+    contour_end = np.r_[contour[1:], contour[0:1]]
+    return np.linalg.norm(contour_end - contour_start, axis=1).sum()
+
+
+def compute_polygon_from_mask(mask):
+    contours = skimage.measure.find_contours(np.pad(mask, pad_width=1))
+    if len(contours) == 0:
+        logger.warning("No contour found, so returning empty polygon.")
+        return np.empty((0, 2), dtype=np.float32)
+
+    contour = max(contours, key=_get_contour_length)
+    POLYGON_APPROX_TOLERANCE = 0.004
+    polygon = skimage.measure.approximate_polygon(
+        coords=contour,
+        tolerance=np.ptp(contour, axis=0).max() * POLYGON_APPROX_TOLERANCE,
+    )
+    polygon = np.clip(polygon, (0, 0), (mask.shape[0] - 1, mask.shape[1] - 1))
+    polygon = polygon[:-1]  # drop last point that is duplicate of first point
+
+    if 0:
+        import PIL.Image
+
+        image_pil = PIL.Image.fromarray(imgviz.gray2rgb(imgviz.bool2ubyte(mask)))
+        imgviz.draw.line_(image_pil, yx=polygon, fill=(0, 255, 0))
+        for point in polygon:
+            imgviz.draw.circle_(image_pil, center=point, diameter=10, fill=(0, 255, 0))
+        imgviz.io.imsave("contour.jpg", np.asarray(image_pil))
+
+    return polygon[:, ::-1]  # yx -> xy
+
+def compute_mask_mix_polygon(mask):
+    # Find contours in the mask
+    contours = skimage.measure.find_contours(np.pad(mask, pad_width=1), level=0.5)
+    if len(contours) == 0:
+        logger.warning("No contour found, so returning empty polygon!")
+        return np.empty((0, 2), dtype=np.float32)
+    
+    polygons = []
+    
+    for contour in contours:
+        POLYGON_APPROX_TOLERANCE = 0.004
+        polygon = skimage.measure.approximate_polygon(
+            coords=contour,
+            tolerance=np.ptp(contour, axis=0).max() * POLYGON_APPROX_TOLERANCE,
+        )
+        polygon = np.clip(polygon, (0, 0), (mask.shape[0] - 1, mask.shape[1] - 1))
+        polygon = polygon[:-1]  # drop last point that is duplicate of first point
+        polygons.append(polygon[:, ::-1])  # yx -> xy
+
+    if 0:
+        import PIL.Image
+
+        image_pil = PIL.Image.fromarray(imgviz.gray2rgb(imgviz.bool2ubyte(mask)))
+        imgviz.draw.line_(image_pil, yx=polygon, fill=(0, 255, 0))
+        for point in polygon:
+            imgviz.draw.circle_(image_pil, center=point, diameter=10, fill=(0, 255, 0))
+        imgviz.io.imsave("contour.jpg", np.asarray(image_pil))
+
+    return polygons
---
+++ labelme/ai/efficient_sam.py
@@ -0,0 +1,121 @@
+import collections
+import threading
+
+import imgviz
+import numpy as np
+import onnxruntime as ort
+import skimage
+
+from ..logger import logger
+from . import _utils
+
+
+class EfficientSam:
+    def __init__(self, encoder_path, decoder_path):
+
+        providers = _utils.get_available_providers() ## added by Alvin
+
+        self._encoder_path = encoder_path ## added by Alvin
+        self._decoder_path = decoder_path ## added by Alvin
+        
+        self._encoder_session = ort.InferenceSession(self._encoder_path, providers=providers) ## added by Alvin
+        self._decoder_session = ort.InferenceSession(self._decoder_path, providers=providers) ## added by Alvin
+
+        self._run_mode = 0 if providers[0] == 'CUDAExecutionProvider' else 1
+        self._lock = threading.Lock()
+        self._image_embedding_cache = collections.OrderedDict()
+
+        self._thread = None
+
+    def set_providers(self,provider): ## added by Alvin
+        providers = _utils.set_providers(provider)
+        self._encoder_session = ort.InferenceSession(self._encoder_path,providers=providers)
+        self._decoder_session = ort.InferenceSession(self._decoder_path,providers=providers)
+        
+        self.set_runMode(providers)
+        logger.info("Mode is modified")
+        
+    def set_runMode(self,device):## added by Alvin
+        self._run_mode = 0  if device == 'CUDAExecutionProvider' else 1
+        
+    def get_runMode(self) :## added by Alvin
+        return self._run_mode
+    
+    def set_image(self, image: np.ndarray):
+        with self._lock:
+            self._image = image
+            self._image_embedding = self._image_embedding_cache.get(
+                self._image.tobytes()
+            )
+
+        if self._image_embedding is None:
+            self._thread = threading.Thread(
+                target=self._compute_and_cache_image_embedding
+            )
+            self._thread.start()
+
+    def _compute_and_cache_image_embedding(self):
+        with self._lock:
+            logger.debug("Computing image ...")
+            image = imgviz.rgba2rgb(self._image)
+            batched_images = image.transpose(2, 0, 1)[None].astype(np.float32) / 255.0
+            (self._image_embedding,) = self._encoder_session.run(
+                output_names=None,
+                input_feed={"batched_images": batched_images},
+            )
+            if len(self._image_embedding_cache) > 10:
+                self._image_embedding_cache.popitem(last=False)
+            self._image_embedding_cache[self._image.tobytes()] = self._image_embedding
+            logger.debug("Done computing image embedding.")
+
+    def _get_image_embedding(self):
+        if self._thread is not None:
+            self._thread.join()
+            self._thread = None
+        with self._lock:
+            return self._image_embedding
+
+    def predict_mask_from_points(self, points, point_labels):
+        return _compute_mask_from_points(
+            decoder_session=self._decoder_session,
+            image=self._image,
+            image_embedding=self._get_image_embedding(),
+            points=points,
+            point_labels=point_labels,
+        )
+
+    def predict_polygon_from_points(self, points, point_labels):
+        mask = self.predict_mask_from_points(points=points, point_labels=point_labels)
+        return _utils.compute_polygon_from_mask(mask=mask)
+
+
+def _compute_mask_from_points(
+    decoder_session, image, image_embedding, points, point_labels
+):
+    input_point = np.array(points, dtype=np.float32)
+    input_label = np.array(point_labels, dtype=np.float32)
+
+    # batch_size, num_queries, num_points, 2
+    batched_point_coords = input_point[None, None, :, :]
+    # batch_size, num_queries, num_points
+    batched_point_labels = input_label[None, None, :]
+
+    decoder_inputs = {
+        "image_embeddings": image_embedding,
+        "batched_point_coords": batched_point_coords,
+        "batched_point_labels": batched_point_labels,
+        "orig_im_size": np.array(image.shape[:2], dtype=np.int64),
+    }
+
+    masks, _, _ = decoder_session.run(None, decoder_inputs)
+    mask = masks[0, 0, 0, :, :]  # (1, 1, 3, H, W) -> (H, W)
+    mask = mask > 0.0
+
+    MIN_SIZE_RATIO = 0.05
+    skimage.morphology.remove_small_objects(
+        mask, min_size=mask.sum() * MIN_SIZE_RATIO, out=mask
+    )
+
+    if 0:
+        imgviz.io.imsave("mask.jpg", imgviz.label2rgb(mask, imgviz.rgb2gray(image)))
+    return mask
